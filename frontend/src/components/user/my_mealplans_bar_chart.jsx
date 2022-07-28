413ee60c1c99ad21846a7c989a7c2a9f5645681f
Wendy Shi <whshiwen@gmail.com>
2022-07-28 15:33:59 -0700

    added alec's nutrition data updates

--- frontend/src/components/user/my_mealplans_bar_chart.jsx
+++ frontend/src/components/user/my_mealplans_bar_chart.jsx
@@ -1,44 +1,75 @@
 import React from "react";
 import Chart from 'chart.js/auto';
 import { Bar } from "react-chartjs-2";
-
-function NutritionData() {
+function NutritionData({ nutrients }) {
+    // console.log('nutirents', {nutrients})
+    let newNutrients = {
+        Calcium: nutrients.calcium_mg * 3 / 1000,
+        Calories: nutrients.calories * 3 / 2250,
+        Carbohydrates: nutrients.carbohydrates_g * 3 / 1300,
+        Fiber: nutrients.dietary_fiber_g * 3 / 25,
+        Fat: nutrients.fat_g * 3 / 44,
+        Iron: nutrients.iron_mg * 3 / 16.3,
+        Protein: nutrients.protein_g * 3 / 56,
+        Sodium: nutrients.sodium_mg * 3 / 2300,
+        Sugars: nutrients.sugars_g * 3 / 125,
+        "Vitamin A": nutrients.vitamin_a_iu_IU * 3 / 3000,
+        "Vitamin C": nutrients.vitamin_c_mg * 3 / 2000
+    }
+    // console.log('newNutirents', newNutrients)
+    // let colors = [ 'rgba(194, 4, 54, 0.7)','rgba(39, 140, 93, 0.7)',]
     const data = {
-        labels: ['Calories', 'Sugar', 'Saturated Fat', 'Carbohydrates', 'Vitamin A', 'Vitamin B', 'Vitamin C', 'Vitamin D', 'Protein'],
+        labels: Object.keys(newNutrients),
         datasets: [{
             indexAxis: 'y',
             label: 'Nutrition Value',
-            data: [65, 35, 80, 81, 56, 55, 40, 70, 95],
+            data: Object.values(newNutrients),
             fill: false,
             backgroundColor: [
-                'rgba(194, 4, 54, 0.7)',
-                'rgba(194, 4, 54, 0.7)',
-                'rgba(194, 4, 54, 0.7)',
-                'rgba(194, 4, 54, 0.7)',
-                'rgba(39, 140, 93, 0.7)',
-                'rgba(39, 140, 93, 0.7)',
-                'rgba(39, 140, 93, 0.7)',
-                'rgba(39, 140, 93, 0.7)',
-                'rgba(39, 140, 93, 0.7)',
+                'rgba(39, 140, 93)',
+                'rgba(39, 140, 93)',
+                'rgba(194, 4, 54)',
+                'rgba(194, 4, 54)',
+                'rgba(194, 4, 54)',
+                'rgba(39, 140, 93)',
+                'rgba(39, 140, 93)',
+                'rgba(194, 4, 54)',
+                'rgba(194, 4, 54)',
+                'rgba(39, 140, 93)',
+                'rgba(39, 140, 93)',
             ],
             borderColor: [
-                'rgb(194, 4, 54)',
-                'rgb(194, 4, 54)',
-                'rgb(194, 4, 54)',
-                'rgb(194, 4, 54)',
-                'rgb(39, 140, 93)',
-                'rgb(39, 140, 93)',
-                'rgb(39, 140, 93)',
-                'rgb(39, 140, 93)',
-                'rgb(39, 140, 93)',
+                'rgba(39, 140, 93)',
+                'rgba(39, 140, 93)',
+                'rgba(194, 4, 54)',
+                'rgba(194, 4, 54)',
+                'rgba(194, 4, 54)',
+                'rgba(39, 140, 93)',
+                'rgba(39, 140, 93)',
+                'rgba(194, 4, 54)',
+                'rgba(194, 4, 54)',
+                'rgba(39, 140, 93)',
+                'rgba(39, 140, 93)',
             ],
-            borderWidth: 1,
+            borderWidth: 1
             // borderHeight: 2
         }]
     };
+    let scales = {
+        xAxes: [{
+            gridLines: {
+                display: false
+            }
+        }],
+        yAxes: [{
+            gridLines: {
+                display: false
+            }
+        }]
+    }
     return (
         <div className="nutrition-data">
-            <Bar data={data} />
+            <Bar data={data} scales={scales} />
         </div>
     )
 }
