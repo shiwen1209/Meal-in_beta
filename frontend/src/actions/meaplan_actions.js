1072530bed029c7646bcdebbb8d9d76e913e993c
shiwen1209 <39010644+shiwen1209@users.noreply.github.com>
2022-07-27 22:54:16 -0700

    add this back in

--- frontend/src/actions/meaplan_actions.js
+++ frontend/src/actions/meaplan_actions.js
@@ -1,17 +1,17 @@
 import * as MpApiUtil from '../util/mp_api_util';
 
-//ALEC COMMENT THIS BACK IN
+ALEC COMMENT THIS BACK IN
 
-// export const RECEIVE_MEALPLAN = "RECEIVE_MEALPLAN";
+export const RECEIVE_MEALPLAN = "RECEIVE_MEALPLAN";
 
-// export const receiveMealplan = (mealplan) => ({
-//     type: RECEIVE_MEALPLAN,
-//     mealplan
-// })
+export const receiveMealplan = (mealplan) => ({
+type: RECEIVE_MEALPLAN,
+ mealplan
+ })
 
-// export const createMealplan = (mealplan) => dispatch => {
-//     return (
-//         MpApiUtil.createMealplan(mealplan)
-//             .then((payload) => dispatch(receiveMealplan(payload.data)))
-//     )
-// }
+ export const createMealplan = (mealplan) => dispatch => {
+     return (
+     MpApiUtil.createMealplan(mealplan)
+             .then((payload) => dispatch(receiveMealplan(payload.data)))
+     )
+ }
